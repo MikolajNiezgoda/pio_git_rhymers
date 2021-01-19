4f582827e1e5c9c6bc2f4eea7b99df085d4a5f65
sublimacja <zuzanna.adamiec@gmail.com>
2021-01-19 20:28:52 +0100

    1.4 - dodanie stalych i annotacji

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int INT = 11;
+    public static final int INT2 =-1;
+    public static final int SIZE= 12;
+    private int[] numbers = new int[SIZE];
 
-    public int total = -1;
+
+    public int total = INT2;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INT2;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == INT;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return INT2;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return INT2;
         return numbers[total--];
     }
 
